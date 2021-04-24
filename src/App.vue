c960403e82f133f1e4fcfa7db7c390f98de5840f
Deodaro <shusilber@gmail.com>
2021-04-24 13:27:54 +1000

    Add all imputs to CustumerForm.vue

--- src/App.vue
+++ src/App.vue
@@ -5,45 +5,19 @@
       <section>
         <div class="containter">
 
-          <!-- first modal -->
-          <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Show first modal</button>
-          <!-- передаём обязательный пропс компонента Modal - title -->
-          <modals
-            title="First modal"
-            v-show="modalFirst"
-            @close="modalFirst = false">
-
-            <div slot="body">
-              <p>text text text text text text</p>
-              <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Well done!</button>
-            </div>
-          </modals>
-
-          <!-- second modal -->
-          <button class="btn btnPrimary" @click="modalSecond.show = !modalSecond.show">Show modal with form</button>
-          <modals
-            title="Modal with form"
-            v-show="modalSecond.show"
-            @close="modalSecond.show = false">
-
-            <div slot="body">
-              <form @submit.prevent="submitSecondForm">
-                <label>Name</label>
-                <input type="text" required v-model="modalSecond.name">
-                <label>Email</label>
-                <input type="email" required v-model="modalSecond.email">
-                <button class="btn btnPrimary">Submit</button>
-              </form> 
-            </div>
-          </modals>
-
           <!-- modal with validate -->
           <button class="btn btnPrimary" @click="modalValidate = !modalValidate">Show modal with form + validate</button>
           <modalValidate v-show="modalValidate" @close="modalValidate = false"/>
 
           <!-- customer form -->
+          <!-- <button class="btn btnPrimary" @click="customerForm = !customerForm">Создать клиента</button>
+          <customerForm v-show="customerForm" @close="customerForm = false" /> -->
+          
           <button class="btn btnPrimary" @click="customerForm = !customerForm">Создать клиента</button>
-          <customerForm v-show="customerForm" @close="customerForm = false" />
+          <customerForm v-show="customerForm" @close="customerForm = false" /> 
+          
+          <!-- <customerForm /> -->
+          
 
         </div>
       </section>
--- src/components/CustomerForm.vue
+++ src/components/CustomerForm.vue
@@ -1,77 +1,179 @@
 <template>
-  <modal
-    title="Создание клиента"
-    @close="$emit('close')">
-    <!-- body -->
-    <div slot="body">
-      <form @submit.prevent="onSubmit">
-        
-        <!-- name -->
-        <!-- класс errorInput появится только тогда, когда есть ошибки (когда error = true) -->
-        <div class="form-item" :class="{ errorInput: $v.name.$error }">
-          <label>Name:</label>
-          <p class="errorText" v-if="!$v.name.required">Field is required!</p>
-          <p class="errorText" v-if="!$v.name.minLength">Name must have at least {{ $v.name.$params.minLength.min }}!</p>
-          <input
-            v-model="name"
-            :class="{ error: $v.name.$error }"
-            @change="$v.name.$touch()">
-        </div>
+  <form @submit.prevent="onSubmit">
 
-        <!-- email -->
-        <div class="form-item" :class="{ errorInput: $v.email.$error }">
-          <label>Email:</label>
-          <p class="errorText" v-if="!$v.email.required">Field is required!</p>
-          <p class="errorText" v-if="!$v.email.email">Email is not correct!</p>
-          <input
-            v-model="email"
-            :class="{ error: $v.email.$error }"
-            @change="$v.email.$touch()">
-        </div>
+    <h1>Создание клиента</h1>
+    <!-- <p><span>Поля, отмеченные *, обязательны для заполнения.</span></p> -->
 
-        <!-- submit button -->
-        <button class="btn btnPrimary">Submit</button>
-     
-      </form> 
+    <!-- surname -->
+    <div class="form-item" :class="{ errorInput: $v.surname.$error }">
+      <label>Фамилия *</label>
+      <p class="errorText" v-if="!$v.surname.required">Пожалуйста, заполните это поле!</p>
+      <input
+        v-model="surname"
+        :class="{ error: $v.surname.$error }"
+        @change="$v.surname.$touch()">
     </div>
-  </modal>
+
+    <!-- name -->
+    <div class="form-item" :class="{ errorInput: $v.name.$error }">
+      <label>Имя *</label>
+      <p class="errorText" v-if="!$v.name.required">Пожалуйста, заполните это поле!</p>
+      <input
+        v-model="name"
+        :class="{ error: $v.name.$error }"
+        @change="$v.name.$touch()">
+    </div>
+    
+    <!-- patronymic -->
+    <div class="form-item">
+      <label>Отчество</label>
+      <input
+        v-model="patronymic"
+        @change="$v.patronymic.$touch()">
+    </div>
+
+    <!-- birthday -->
+    <div class="form-item" :class="{ errorInput: $v.birthday.$error }">
+      <label>Дата рождения *</label>
+      <p class="errorText" v-if="!$v.birthday.required">Пожалуйста, заполните это поле!</p>
+      <input
+        type="date"
+        v-model="birthday"
+        :class="{ error: $v.birthday.$error }"
+        @change="$v.birthday.$touch()">
+    </div>
+
+    <!-- tel -->
+    <div class="form-item" :class="{ errorInput: $v.tel.$error }">
+      <label>Номер телефона *</label>
+      <p class="errorText" v-if="!$v.tel.required">Пожалуйста, заполните это поле!</p>
+      <!-- <p class="errorText" v-if="!$v.tel.numeric">Укажите номер в формате 7 999 999 9999</p> -->
+      <!-- <p class="errorText" v-if="!$v.tel.isPhone">Укажите номер в формате 7 999 999 9999</p> -->
+      <p class="errorText" v-if="!$v.tel.between">Укажите номер в формате 7999999999</p>
+      <!-- <p class="errorText" v-if="!$v.tel.tel">Указан неверный номер телефона</p> -->
+      <p class="errorText" v-if="!$v.tel.minLength">Укажите {{ $v.tel.$params.minLength.min }} цифр в номере телефона</p>
+      <!-- <p class="errorText" v-if="!$v.tel.minLength">Укажите номер в формете +7 (___) ___ ____</p> -->
+      <input
+        type="tel"
+        placeholder="7999999999"
+        v-model="tel"
+        :class="{ error: $v.tel.$error }"
+        @change="$v.tel.$touch()">
+    </div>
+
+    <!-- gender -->
+    <div class="form-item">
+      <label>Пол</label> 
+        <select
+          v-model="gender"
+          @change="$v.gender.$touch()">
+          <option value="female">женский</option>
+          <option value="male">мужской</option>
+        </select>
+    </div>
+
+    <!-- group -->
+    <div class="form-item" :class="{ errorInput: $v.group.$error }">
+      <label>Группа клиентов *</label>
+      <p class="errorText" v-if="!$v.group.required">Пожалуйста, заполните это поле!</p>
+      <select
+        multiple
+        name=""
+        id=""
+        v-model="group"
+        :class="{ error: $v.group.$error }"
+        @change="$v.group.$touch()">
+        <option value="vip">VIP</option>
+        <option value="difficult">Проблемные</option>
+        <option value="oms">ОМС</option>
+      </select>
+    </div>
+
+    <!-- doctor -->
+    <div class="form-item">
+      <label>Лечащий врач</label>
+      <select
+        v-model="doctor"
+        @change="$v.doctor.$touch()">
+        <option value="Ivanov">Иванов</option>
+        <option value="Zakharov">Захаров</option>
+        <option value="Chernysheva">Чернышева</option>
+      </select>
+    </div>
+
+    <!-- not send sms-->
+    <div class="form-item">
+      <label>Не отправлять СМС</label>
+      <input
+        type="checkbox"
+        v-model="notSms"
+        @change="$v.notSms.$touch()">
+    </div>
+
+    <!-- submit button -->
+    <button class="btn btnPrimary">Submit</button>
+  </form>
 </template>
 
 <script>
-import { required, minLength, email } from 'vuelidate/lib/validators'
-
+import { required, minLength, numeric, between } from 'vuelidate/lib/validators'
 import modal from '@/components/UI/Modal.vue'
 
 export default {
   components: { modal },
   data () {
     return {
+      surname: '',
       name: '',
-      email: ''
+      patronymic: '',
+      birthday: '',
+      tel: '',
+      gender: '',
+      group: [],
+      doctor: '',
+      notSms: ''
     }
   },
   validations: {
-    name: {
+    surname: { required },
+    name: { required },
+    birthday: { required },
+    tel: {
       required,
-      minLength: minLength(4)
+      numeric,
+      minLength: minLength(11),
+      between: between(70000000000, 79999999999)
     },
-    email: {
-      required,
-      email
-    }
+    gender: {},
+    group: { required },
+    doctor: {},
+    notSms: {}
   },
   methods: {
     onSubmit () {
       this.$v.$touch()
       if (!this.$v.$invalid) {
         const user = {
+          surname: this.surname,
           name: this.name,
-          email: this.email
+          patronymic: this.patronymic,
+          birthday: this.birthday,
+          tel: this.tel,
+          gender: this.gender,
+          group: this.group,
+          doctor: this.doctor,
+          notSms: this.notSms
         }
         console.log(user)
         // reset and close
+        this.surname = ''
         this.name = ''
-        this.email = ''
+        this.patronymic = ''
+        this.birthday = ''
+        this.tel = ''
+        this.gender = ''
+        this.group = []
+        this.doctor = ''
         this.$v.$reset()
         this.$emit('close')
       }
